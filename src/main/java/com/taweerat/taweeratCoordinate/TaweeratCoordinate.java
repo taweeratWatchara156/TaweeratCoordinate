c1c58250d78f7f91e549f7b545c5ff9c2edc2ea2
GangSom <129844552+taweeratWatchara156@users.noreply.github.com>
2024-07-29 22:03:02 +0700

    remove unused import / a little bit with variable

--- src/main/java/com/taweerat/taweeratCoordinate/TaweeratCoordinate.java
+++ src/main/java/com/taweerat/taweeratCoordinate/TaweeratCoordinate.java
@@ -5,12 +5,10 @@ import com.taweerat.taweeratCoordinate.listerners.Coordinate;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.text.DecimalFormat;
 import java.util.*;
 
 public final class TaweeratCoordinate extends JavaPlugin {
@@ -47,8 +45,7 @@ public final class TaweeratCoordinate extends JavaPlugin {
         long minutes = (ticks % 1000) * 60 / 1000;
 
         // Format hours and minutes to HH:MM format
-        String formattedTime = String.format("%02d:%02d", hours, minutes);
-        return formattedTime;
+        return String.format("%02d:%02d", hours, minutes);
     }
 
     private List<String> loadConfig(){
